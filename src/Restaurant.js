be63ce5e7ab69e548c606fc3783ac44eb86a1b06
Whymess <Jonathan.lev.messing@gmail.com>
2017-10-18 16:00:27 -0400

    Voting displaysgit add .git add .

--- src/Restaurant.js
+++ src/Restaurant.js
@@ -4,10 +4,16 @@ import './Restaurant.css';
 
 class Restaurant extends Component {
   render () {
-     const {name, handleSelect, handleDeslect} = this.props
+     const {name, votes, handleSelect, handleDeslect} = this.props
     return (
       <article className="Restaurant">
         <h3> {name} </h3>
+        <ul>
+
+          {votes && map(votes, (vote, key) => <li key={key}> {vote}</li> )
+          }
+
+        </ul>
         <button onClick={handleSelect}> 
             Yea, I'd go there.... 
         </button>
